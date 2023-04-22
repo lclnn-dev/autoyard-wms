e8bcda7f4819ede5df2d4dbb9f2c63f84361d492
LucyLenn <ov.lclnn@gmail.com>
2023-04-22 20:33:24 +0300

    init commit

---
+++ src/main/java/com/autoyardproject/com/AutoyardApplication.java
@@ -0,0 +1,13 @@
+package com.autoyardproject.com;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AutoyardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoyardApplication.class, args);
+    }
+}
